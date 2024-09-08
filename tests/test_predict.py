4706a17861a25a7e552681f6bdc081ef17739f3f
Raymond Huang <1415176717@qq.com>
2024-09-08 14:23:44 -0700

    test case for code and writing

--- tests/test_predict.py
+++ tests/test_predict.py
@@ -1,10 +1,17 @@
 from keywordsai_eval.backends.redis_stack import RedisEvaluationBackend
 
-def test_predict():
+def test_code_predict():
     kai_eval = RedisEvaluationBackend()
     result = kai_eval.predict("How to code \"Hello worlds\" in Python")
 
     assert result == "Coding", f"The result was {result} instead of Coding"
 
+def test_writing_predict():
+    kai_eval = RedisEvaluationBackend()
+    result = kai_eval.predict("How to write a novel")
+
+    assert result == "Writing", f"The result was {result} instead of Writing"
+
 if __name__ == "__main__":
-    test_predict()
+    test_code_predict()
+    test_writing_predict()
